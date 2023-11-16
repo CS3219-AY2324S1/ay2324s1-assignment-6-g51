ec941e561becfaa6600b6c9ce5df6621681bacee
Haiqel <34855234+Acerizm@users.noreply.github.com>
2023-11-16 19:18:36 +0800

    Update

--- getLeetCodeQns/index.js
+++ getLeetCodeQns/index.js
@@ -9,48 +9,63 @@ functions.http('helloHttp', async (req, res) => {
     // Extract data from the response
     const leetCodeData = response.data;
     let questions;
+    let result = [];
     // Check if the data has the expected structure
     if (leetCodeData && leetCodeData.stat_status_pairs) {
       // Process each question in the response
       questions = leetCodeData.stat_status_pairs.map(question => {
-        return {
-          title: question.stat.question__title_slug,
-          difficulty: question.difficulty.level,
-          totalAccepted: question.stat.total_acs,
-          totalSubmitted: question.stat.total_submitted,
-        };
-      });
+            return {
+            title: question.stat.question__title_slug,
+            difficulty: question.difficulty.level,
+            totalAccepted: question.stat.total_acs,
+            totalSubmitted: question.stat.total_submitted,
+            };
+        });
 
-    const response = await axios.post(
-    'https://leetcode.com/graphql/',
-    {
-        query: `
-        query getQuestionDetail($titleSlug: String!) {
-            question(titleSlug: $titleSlug) {
-            content
+        i = 0
+        while (i < 10) {
+
+            // Fetch the question description for the first question
+            const graphqlResponse = await axios.post(
+                'https://leetcode.com/graphql/',
+                {
+                query: `
+                    query getQuestionDetail($titleSlug: String!) {
+                        question(titleSlug: $titleSlug) {
+                            content
+                        }
+                    }
+                `,
+                variables: {
+                    titleSlug: questions[50].title,
+                },
+                },
+                {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                }
+            );
+
+            console.log('GraphQL Response:', graphqlResponse.data); // Log the GraphQL response
+
+            const {content, difficulty, title,} = graphqlResponse.data.data.question;
+            resultQuestion = {
+                title: title,
+                description: content,
+                difficulty: difficulty
             }
+            result.push(resultQuestion);
+            i++;
         }
-        `,
-        variables: {
-        titleSlug: questions[0].title,
-        },
-    },
-    {
-        headers: {
-        'Content-Type': 'application/json',
-        },
+        res.status(200).send(result);
+    } else {
+      console.error('Invalid data structure in the LeetCode API response');
+      res.status(500).send('Internal Server Error');
     }
-    );  
-
-    const questionDescription = response.data.data.question.content;
-    console.log(questionDescription)
-    res.status(200).send(questionDescription)
-  } else {
-    console.error('Invalid data structure in the LeetCode API response');
-  }
   } catch (error) {
     console.error('Error making API request:', error);
     res.status(500).send('Internal Server Error');
   }
-
 });
+
