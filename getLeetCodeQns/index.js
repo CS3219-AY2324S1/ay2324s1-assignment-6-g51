ead039ccb9587833f8cc56a33195b9897c13ee03
Kang-Quan <KangQuan_lee@yahoo.com.sg>
2023-11-17 01:27:55 +0800

    Add functionality where function can pull qns from leetcode to database

--- getLeetCodeQns/index.js
+++ getLeetCodeQns/index.js
@@ -1,7 +1,22 @@
 const functions = require('@google-cloud/functions-framework');
 const axios = require('axios');
+const { initializeApp } = require("firebase/app");
+const { getDatabase,ref,set, remove } = require("firebase/database");
 
 functions.http('helloHttp', async (req, res) => {
+    const firebaseConfig = {
+        // ...
+        // The value of `databaseURL` depends on the location of the database
+        databaseURL: "https://primeval-hearth-399015-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    };
+        
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    
+    
+    // Initialize Realtime Database and get a reference to the service
+    const database = getDatabase(app);
+    
   try {
     // Make a GET request to the LeetCode API
     const response = await axios.get('https://leetcode.com/api/problems/all/');
@@ -23,7 +38,7 @@ functions.http('helloHttp', async (req, res) => {
         });
 
         i = 0
-        while (i < 10) {
+        while (i < 100) {
 
             // Fetch the question description for the first question
             const graphqlResponse = await axios.post(
@@ -37,7 +52,7 @@ functions.http('helloHttp', async (req, res) => {
                     }
                 `,
                 variables: {
-                    titleSlug: questions[50].title,
+                    titleSlug: questions[i].title,
                 },
                 },
                 {
@@ -47,9 +62,21 @@ functions.http('helloHttp', async (req, res) => {
                 }
             );
 
-            console.log('GraphQL Response:', graphqlResponse.data); // Log the GraphQL response
+            //console.log('GraphQL Response:', graphqlResponse); // Log the GraphQL response
 
-            const {content, difficulty, title,} = graphqlResponse.data.data.question;
+            const content = graphqlResponse.data.data.question.content;
+            const title = questions[50].title;
+            //console.log(title);
+            const difficultyLvl = questions[50].difficulty;
+            let difficulty = null;
+            if (difficultyLvl === 3) {
+                difficulty = "Hard"
+            } else if (difficultyLvl === 2) {
+                difficulty = "Medium"
+            } else {
+                difficulty = "Easy"
+            }
+
             resultQuestion = {
                 title: title,
                 description: content,
@@ -57,6 +84,27 @@ functions.http('helloHttp', async (req, res) => {
             }
             result.push(resultQuestion);
             i++;
+        }
+
+        const rootRef = ref(database);
+        remove(rootRef)
+            .then(() => {
+                console.log('All entries removed successfully.');
+        })
+            .catch((error) => {
+                console.error('Error removing entries:', error);
+        });
+        for (let j = 0; j < result.length; j++) {
+            currQn = result[j];
+            currTitle = currQn.title;
+            currDes = currQn.description;
+            currDif = currQn.difficulty;
+            
+            if (currTitle && currDes && currDif) {
+                writeUserData(j + 1, currTitle, currDes, currDif, database);
+            }
+
+
         }
         res.status(200).send(result);
     } else {
@@ -69,3 +117,10 @@ functions.http('helloHttp', async (req, res) => {
   }
 });
 
+function writeUserData(qnId, title, description, difficulty, db) {
+    set(ref(db, 'users/' + qnId), {
+      title: title,
+      description: description,
+      difficulty : difficulty
+    });
+  }
